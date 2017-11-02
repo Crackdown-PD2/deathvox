cab4293c4302282c98f0c0f669b134bdf00a3777
iamgoofball <iamgoofball@gmail.com>
2017-11-01 21:55:48 -0700

    fixes downs and tasers

---
+++ lua/lib/units/beings/player/playerdamage.lua
@@ -0,0 +1,154 @@
+function PlayerDamage:init(unit)
+	self._lives_init = tweak_data.player.damage.LIVES_INIT
+	self._lives_init = managers.crime_spree:modify_value("PlayerDamage:GetMaximumLives", self._lives_init)
+	self._unit = unit
+	self._max_health_reduction = managers.player:upgrade_value("player", "max_health_reduction", 1)
+	self._healing_reduction = managers.player:upgrade_value("player", "healing_reduction", 1)
+	self._revives = Application:digest_value(0, true)
+	self._uppers_elapsed = 0
+
+	self:replenish()
+
+	local player_manager = managers.player
+	self._bleed_out_health = Application:digest_value(tweak_data.player.damage.BLEED_OUT_HEALTH_INIT * player_manager:upgrade_value("player", "bleed_out_health_multiplier", 1), true)
+	self._god_mode = Global.god_mode
+	self._invulnerable = false
+	self._mission_damage_blockers = {}
+	self._gui = Overlay:newgui()
+	self._ws = self._gui:create_screen_workspace()
+	self._focus_delay_mul = 1
+	self._dmg_interval = tweak_data.player.damage.MIN_DAMAGE_INTERVAL
+	self._next_allowed_dmg_t = Application:digest_value(-100, true)
+	self._last_received_dmg = 0
+	self._next_allowed_sup_t = -100
+	self._last_received_sup = 0
+	self._supperssion_data = {}
+	self._inflict_damage_body = self._unit:body("inflict_reciever")
+
+	self._inflict_damage_body:set_extension(self._inflict_damage_body:extension() or {})
+
+	local body_ext = PlayerBodyDamage:new(self._unit, self, self._inflict_damage_body)
+	self._inflict_damage_body:extension().damage = body_ext
+
+	managers.sequence:add_inflict_updator_body("fire", self._unit:key(), self._inflict_damage_body:key(), self._inflict_damage_body:extension().damage)
+
+	self._doh_data = tweak_data.upgrades.damage_to_hot_data or {}
+	self._damage_to_hot_stack = {}
+	self._armor_stored_health = 0
+	self._can_take_dmg_timer = 0
+	self._regen_on_the_side_timer = 0
+	self._regen_on_the_side = false
+	self._interaction = managers.interaction
+	self._armor_regen_mul = managers.player:upgrade_value("player", "armor_regen_time_mul", 1)
+	self._dire_need = managers.player:has_category_upgrade("player", "armor_depleted_stagger_shot")
+	self._has_damage_speed = managers.player:has_inactivate_temporary_upgrade("temporary", "damage_speed_multiplier")
+	self._has_damage_speed_team = managers.player:upgrade_value("player", "team_damage_speed_multiplier_send", 0) ~= 0
+
+
+	local function revive_player()
+		self:revive(true)
+	end
+
+	managers.player:register_message(Message.RevivePlayer, self, revive_player)
+
+	self._current_armor_fill = 0
+	local has_swansong_skill = player_manager:has_category_upgrade("temporary", "berserker_damage_multiplier")
+	self._current_state = nil
+	self._listener_holder = unit:event_listener()
+
+	if player_manager:has_category_upgrade("player", "damage_to_armor") then
+		local damage_to_armor_data = player_manager:upgrade_value("player", "damage_to_armor", nil)
+		local armor_data = tweak_data.blackmarket.armors[managers.blackmarket:equipped_armor(true, true)]
+
+		if damage_to_armor_data and armor_data then
+			local idx = armor_data.upgrade_level
+			self._damage_to_armor = {
+				armor_value = damage_to_armor_data[idx][1],
+				target_tick = damage_to_armor_data[idx][2],
+				elapsed = 0
+			}
+
+
+			local function on_damage(damage_info)
+				local attacker_unit = damage_info and damage_info.attacker_unit
+
+				if alive(attacker_unit) and attacker_unit:base() and attacker_unit:base().thrower_unit then
+					attacker_unit = attacker_unit:base():thrower_unit()
+				end
+
+				if self._unit == attacker_unit then
+					local time = Application:time()
+
+					if self._damage_to_armor.target_tick < time - self._damage_to_armor.elapsed then
+						self._damage_to_armor.elapsed = time
+
+						self:restore_armor(self._damage_to_armor.armor_value, true)
+					end
+				end
+			end
+
+			CopDamage.register_listener("on_damage", {"on_damage"}, on_damage)
+		end
+	end
+
+	self._listener_holder:add("on_use_armor_bag", {"on_use_armor_bag"}, callback(self, self, "_on_use_armor_bag_event"))
+
+	if self:_init_armor_grinding_data() then
+
+		function self._on_damage_callback_func()
+			return callback(self, self, "_on_damage_armor_grinding")
+		end
+
+		self:_add_on_damage_event()
+		self._listener_holder:add("on_enter_bleedout", {"on_enter_bleedout"}, callback(self, self, "_on_enter_bleedout_event"))
+
+		if has_swansong_skill then
+			self._listener_holder:add("on_enter_swansong", {"on_enter_swansong"}, callback(self, self, "_on_enter_swansong_event"))
+			self._listener_holder:add("on_exit_swansong", {"on_enter_bleedout"}, callback(self, self, "_on_exit_swansong_event"))
+		end
+
+		self._listener_holder:add("on_revive", {"on_revive"}, callback(self, self, "_on_revive_event"))
+	else
+		self:_init_standard_listeners()
+	end
+
+	if player_manager:has_category_upgrade("temporary", "revive_damage_reduction") then
+		self._listener_holder:add("combat_medic_damage_reduction", {"on_revive"}, callback(self, self, "_activate_combat_medic_damage_reduction"))
+	end
+
+	if player_manager:has_category_upgrade("player", "revive_damage_reduction") and player_manager:has_category_upgrade("player", "revive_damage_reduction") then
+
+		local function on_revive_interaction_start()
+			managers.player:set_property("revive_damage_reduction", player_manager:upgrade_value("player", "revive_damage_reduction"), 1)
+		end
+
+
+		local function on_exit_interaction()
+			managers.player:remove_property("revive_damage_reduction")
+		end
+
+
+		local function on_revive_interaction_success()
+			managers.player:activate_temporary_upgrade("temporary", "revive_damage_reduction")
+		end
+
+		self._listener_holder:add("on_revive_interaction_start", {"on_revive_interaction_start"}, on_revive_interaction_start)
+		self._listener_holder:add("on_revive_interaction_interrupt", {"on_revive_interaction_interrupt"}, on_exit_interaction)
+		self._listener_holder:add("on_revive_interaction_success", {"on_revive_interaction_success"}, on_revive_interaction_success)
+	end
+
+	managers.mission:add_global_event_listener("player_regenerate_armor", {"player_regenerate_armor"}, callback(self, self, "_regenerate_armor"))
+	managers.mission:add_global_event_listener("player_force_bleedout", {"player_force_bleedout"}, callback(self, self, "force_into_bleedout", false))
+
+	local level_tweak = tweak_data.levels[managers.job:current_level_id()]
+
+	if level_tweak and level_tweak.is_safehouse and not level_tweak.is_safehouse_combat then
+		self:set_mission_damage_blockers("damage_fall_disabled", true)
+		self:set_mission_damage_blockers("invulnerable", true)
+	end
+
+	self._delayed_damage = {
+		epsilon = 0.001,
+		chunks = {}
+	}
+end
