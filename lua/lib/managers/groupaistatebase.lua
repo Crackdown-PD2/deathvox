75c64d0644a667a9e8a318a15bd3b2c94ee89386
Iamgoofball <iamgoofball@gmail.com>
2017-11-03 11:30:25 -0700

    crash fix test

--- lua/lib/managers/groupaistatebase.lua
+++ lua/lib/managers/groupaistatebase.lua
@@ -23,3 +23,42 @@ function GroupAIStateBase:on_simulation_started()
 		medic = true
 	}
 end
+
+function GroupAIStateBase:detonate_world_smoke_grenade(id)
+	self._smoke_grenades = self._smoke_grenades or {}
+
+	if not self._smoke_grenades[id] then
+		Application:error("Could not detonate smoke grenade as it was not queued!", id)
+
+		return
+	end
+
+	local data = self._smoke_grenades[id]
+
+	if data.flashbang then
+		if Network:is_client() then
+			return
+		end
+
+		local flashbang_unit = "units/payday2/weapons/wpn_frag_flashbang/wpn_frag_flashbang"
+		local pos = data.detonate_pos + Vector3(0, 0, 1)
+		local rotation = Rotation(math.random() * 360, 0, 0)
+		local flash_grenade = World:spawn_unit(Idstring(flashbang_unit), data.detonate_pos, rotation)
+
+		flash_grenade:base():activate(data.detonate_pos, data.duration)
+
+		self._smoke_grenades[id] = nil
+	else
+		data.duration = data.duration == 0 and 15 or data.duration
+		local smoke_grenade = World:spawn_unit(Idstring("units/weapons/smoke_grenade_quick/smoke_grenade_quick"), data.detonate_pos, Rotation())
+
+		smoke_grenade:base():activate(data.detonate_pos, data.duration)
+		managers.groupai:state():teammate_comment(nil, "g40x_any", data.detonate_pos, true, 2000, false)
+
+		data.grenade = smoke_grenade
+	end
+
+	if Network:is_server() then
+		managers.network:session():send_to_peers_synched("sync_smoke_grenade", data.detonate_pos, data.detonate_pos, data.duration, data.flashbang and true or false)
+	end
+end
