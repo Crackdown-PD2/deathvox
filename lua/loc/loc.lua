5ff1018846c77af27142c4ada8a53dd444e95c00
iamgoofball <iamgoofball@gmail.com>
2017-12-09 16:50:40 -0800

    corrects name

--- lua/loc/loc.lua
+++ lua/loc/loc.lua
@@ -1,6 +1,6 @@
 Hooks:Add("LocalizationManagerPostInit", "DeathVox_Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-		["menu_risk_sm_wish"] = "Bad news, I'm your doctor.",
-		["menu_difficulty_sm_wish"] = "GOOD NEWS, FREE HEALTHCARE"
+		["menu_risk_sm_wish"] = "It's true.",
+		["menu_difficulty_sm_wish"] = "ANIME IS TRASH"
 	})
 end)
