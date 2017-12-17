9e39cecec7840ebd3c33db4cef88c5faf4fa8683
Iamgoofball <iamgoofball@gmail.com>
2017-12-16 21:21:55 -0800

    Update loc.lua

--- lua/loc/loc.lua
+++ lua/loc/loc.lua
@@ -1,6 +1,6 @@
 Hooks:Add("LocalizationManagerPostInit", "DeathVox_Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-		["menu_risk_sm_wish"] = "It's true.",
-		["menu_difficulty_sm_wish"] = "ANIME IS TRASH"
+		["menu_risk_sm_wish"] = "Fo Sho.",
+		["menu_difficulty_sm_wish"] = "Crackdown"
 	})
 end)
