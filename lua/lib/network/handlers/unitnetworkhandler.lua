088d99dba850dad73ceca73adf65d6562befcb2b
iamgoofball <iamgoofball@gmail.com>
2017-12-23 19:34:30 -0800

    pop it

--- lua/lib/network/handlers/unitnetworkhandler.lua
+++ lua/lib/network/handlers/unitnetworkhandler.lua
@@ -5,3 +5,34 @@ function UnitNetworkHandler:sync_cs_grenade(detonate_pos, shooter_pos, duration,
 
 	managers.groupai:state():sync_cs_grenade(detonate_pos, shooter_pos, duration, damage, d_rad)
 end
+
+function UnitNetworkHandler:sync_tear_gas_grenade_properties(grenade, radius, damage, duration, location)
+	if grenade then
+		grenade:base():set_properties({
+			radius = radius,
+			damage = damage,
+			duration = duration
+		})
+	else
+		grenade = World:spawn_unit(Idstring("units/weapons/cs_grenade_quick/cs_grenade_quick"), location, Rotation())
+		grenade:base():set_properties({
+			radius = radius,
+			damage = damage,
+			duration = duration
+		})
+	end
+end
+
+function UnitNetworkHandler:sync_tear_gas_grenade_detonate(grenade, radius, damage, duration, location)
+	if grenade then
+		grenade:base():detonate()
+	else
+		grenade = World:spawn_unit(Idstring("units/weapons/cs_grenade_quick/cs_grenade_quick"), location, Rotation())
+		grenade:base():set_properties({
+			radius = radius,
+			damage = damage,
+			duration = duration
+		})
+		grenade:base():detonate()
+	end
+end
