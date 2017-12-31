3f5b0c1e6230ebcea1bd323be622dbfb452008bd
Campbell Suter <znix@znix.xyz>
2017-12-31 19:06:28 +1300

    Remove dependency on BeardLib

--- Core.lua
+++ Core.lua
@@ -1,16 +1,6 @@
-if not ModCore then
-	log("[ERROR] Unable to find ModCore from BeardLib! Is BeardLib installed correctly?")
-	return
-end
-DeathVox = DeathVox or class(ModCore)
-
-function DeathVox:init()
-	self.super.init(self, ModPath .. "config.xml", true, true)
-end
-
 
 if not _G.deathvox then
-	local success, err = pcall(function() _G.deathvox = DeathVox:new() end)
+	_G.deathvox = {}
 	_G.deathvox.ModPath = ModPath
 	blt.xaudio.setup()
 	_G.deathvox.BufferedSounds = {
