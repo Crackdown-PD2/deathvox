4d661093648ac05336b4ce73df0cd29de9b7a32b
DiscendoVox <marriotr@gmail.com>
2018-04-28 22:24:35 -0400

    General enemy framework/documentation

--- lua/lib/managers/mission/elementspawnenemydummy.lua
+++ lua/lib/managers/mission/elementspawnenemydummy.lua
@@ -1,11 +1,16 @@
 local sm_wish = {
 -- this version last edited by vox Feb 26 2018.
+	
+-- Beat police.
+-- need to populate as necesary.
+	
 --city swats.
 		["units/payday2/characters/ene_city_swat_1/ene_city_swat_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/payday2/characters/ene_city_swat_2/ene_city_swat_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightshot/ene_deathvox_lightshot",
 		["units/payday2/characters/ene_city_swat_3/ene_city_swat_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/payday2/characters/ene_city_heavy_r870/ene_city_heavy_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_heavyshot/ene_deathvox_heavyshot",
 		["units/payday2/characters/ene_city_heavy_g36/ene_city_heavy_g36"] = "units/pd2_mod_gageammo/characters/ene_deathvox_heavyar/ene_deathvox_heavyar",
+
 --fbi swats.
 		["units/payday2/characters/ene_fbi_swat_1/ene_fbi_swat_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/payday2/characters/ene_fbi_swat_2/ene_fbi_swat_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightshot/ene_deathvox_lightshot",
@@ -17,11 +22,45 @@ local sm_wish = {
 		["units/payday2/characters/ene_swat_2/ene_swat_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightshot/ene_deathvox_lightshot",
 		["units/payday2/characters/ene_swat_heavy_1/ene_swat_heavy_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_heavyar/ene_deathvox_heavyar",
 		["units/payday2/characters/ene_swat_heavy_r870/ene_swat_heavy_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_heavyshot/ene_deathvox_heavyshot",
+
 -- base zeals.
 		["units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy/ene_zeal_swat_heavy"] = "units/pd2_mod_gageammo/characters/ene_deathvox_heavyar/ene_deathvox_heavyar",
+
+-- SWAT special enemies.
+-- shields.
+		["units/payday2/characters/ene_shield_1/ene_shield_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
+		["units/payday2/characters/ene_shield_2/ene_shield_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
+		["units/payday2/characters/ene_city_shield/ene_city_shield"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
+		["units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
+--cloakers.
+		["units/payday2/characters/ene_spook_1/ene_spook_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_cloaker/ene_deathvox_cloaker",
+		["units/pd2_dlc_gitgud/characters/ene_zeal_cloaker/ene_zeal_cloaker"] = "units/pd2_mod_gageammo/characters/ene_deathvox_cloaker/ene_deathvox_cloaker",
+--medics.
+		["units/payday2/characters/ene_medic_m4/ene_medic_m4"] = "units/pd2_mod_gageammo/characters/ene_deathvox_medic/ene_deathvox_medic",
+		["units/payday2/characters/ene_medic_r870/ene_medic_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_medic/ene_deathvox_medic",
+--snipers. commenting out in case we get custom unit appearance.
+--		["units/payday2/characters/ene_sniper_1/ene_sniper_1"] = 
+--		["units/payday2/characters/ene_sniper_2/ene_sniper_2"] = 
+--taser.
+		["units/payday2/characters/ene_tazer_1/ene_tazer_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_taser/ene_deathvox_taser",
+--bulldozers.
+		["units/payday2/characters/ene_bulldozer_1/ene_bulldozer_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
+		["units/payday2/characters/ene_bulldozer_2/ene_bulldozer_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
+		["units/payday2/characters/ene_bulldozer_3/ene_bulldozer_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
+	
+--Z-dozers. According to the spawner the order's different on OD. I've replicated it. Need to check into this though.
+		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer/ene_zeal_bulldozer"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
+		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer_2/ene_zeal_bulldozer_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
+		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer_3/ene_zeal_bulldozer_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
+
+-- Medicdozer, in case of scripted use in future.
+--		["units/pd2_dlc_drm/characters/ene_bulldozer_medic/ene_bulldozer_medic"] = 	
+-- Minidozer, in case of scripted use in future.
+--		["units/pd2_dlc_drm/characters/ene_bulldozer_minigun/ene_bulldozer_minigun"] = 
 -- sm_ enemies currently unused.
 
+-- Akan below.	
 -- akan cop enemies.
 		["units/pd2_dlc_mad/characters/ene_akan_cs_cop_ak47_ass/ene_akan_cs_cop_ak47_ass"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/pd2_dlc_mad/characters/ene_akan_cs_cop_akmsu_smg/ene_akan_cs_cop_akmsu_smg"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
@@ -41,66 +80,65 @@ local sm_wish = {
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_swat_dw_ak47_ass/ene_akan_fbi_swat_dw_ak47_ass"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_swat_dw_r870/ene_akan_fbi_swat_dw_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightshot/ene_deathvox_lightshot",
 
---special enemies.
--- shields.
-		["units/payday2/characters/ene_shield_1/ene_shield_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
-		["units/payday2/characters/ene_shield_2/ene_shield_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
-		["units/payday2/characters/ene_city_shield/ene_city_shield"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
-		["units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
+-- Akan Specials below.
 -- akan shields.
 		["units/pd2_dlc_mad/characters/ene_akan_cs_shield_c45/ene_akan_cs_shield_c45"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_shield_dw_sr2_smg/ene_akan_fbi_shield_dw_sr2_smg"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_shield_sr2_smg/ene_akan_fbi_shield_sr2_smg"] = "units/pd2_mod_gageammo/characters/ene_deathvox_shield/ene_deathvox_shield",
 
---cloakers.
-		["units/payday2/characters/ene_spook_1/ene_spook_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_cloaker/ene_deathvox_cloaker",
-		["units/pd2_dlc_gitgud/characters/ene_zeal_cloaker/ene_zeal_cloaker"] = "units/pd2_mod_gageammo/characters/ene_deathvox_cloaker/ene_deathvox_cloaker",
 -- akan cloaker.
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_spooc_asval_smg/ene_akan_fbi_spooc_asval_smg"] = "units/pd2_mod_gageammo/characters/ene_deathvox_cloaker/ene_deathvox_cloaker",
---medics.
-		["units/payday2/characters/ene_medic_m4/ene_medic_m4"] = "units/pd2_mod_gageammo/characters/ene_deathvox_medic/ene_deathvox_medic",
-		["units/payday2/characters/ene_medic_r870/ene_medic_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_medic/ene_deathvox_medic",
---snipers. commenting out in case we get custom unit appearance.
---		["units/payday2/characters/ene_sniper_1/ene_sniper_1"] = 
---		["units/payday2/characters/ene_sniper_2/ene_sniper_2"] = 
--- akan sniper.
---		missing akan sniper.
 
---taser.
-		["units/payday2/characters/ene_tazer_1/ene_tazer_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_taser/ene_deathvox_taser",
+-- akan sniper. Not swapping for now. Need to see how to get effect working regardless.
+--		["units/pd2_dlc_mad/characters/ene_akan_cs_swat_sniper_svd_snp/ene_akan_cs_swat_sniper_svd_snp"]
+
 -- akan taser.
 		["units/pd2_dlc_mad/characters/ene_akan_cs_tazer_ak47_ass/ene_akan_cs_tazer_ak47_ass"] = "units/pd2_mod_gageammo/characters/ene_deathvox_taser/ene_deathvox_taser",
 
---bulldozers.
-		["units/payday2/characters/ene_bulldozer_1/ene_bulldozer_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
-		["units/payday2/characters/ene_bulldozer_2/ene_bulldozer_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
-		["units/payday2/characters/ene_bulldozer_3/ene_bulldozer_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
---according to the spawner the order's different on OD. I've replicated it. Need to check into this though.
-		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer/ene_zeal_bulldozer"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
-		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer_2/ene_zeal_bulldozer_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
-		["units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer_3/ene_zeal_bulldozer_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
 --akan dozers.
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_tank_r870/ene_akan_fbi_tank_r870"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_tank_rpk_lmg/ene_akan_fbi_tank_rpk_lmg"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
 		["units/pd2_dlc_mad/characters/ene_akan_fbi_tank_saiga/ene_akan_fbi_tank_saiga"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
--- need to add medic and mini too.
 
---not the fbis, they're one of the units players want to see back. We may adjust and sub in a new weaponpreset for them though.
---		["units/payday2/characters/ene_fbi_1/ene_fbi_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
---		["units/payday2/characters/ene_fbi_2/ene_fbi_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
---		["units/payday2/characters/ene_fbi_3/ene_fbi_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lightar/ene_deathvox_lightar",
+-- Zombie cops below.
+	
+-- Zombie Special units.
+-- Zombie Dozers.
+--		["units/pd2_dlc_hvh/characters/ene_bulldozer_hvh_1/ene_bulldozer_hvh_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_greendozer/ene_deathvox_greendozer",
+--		["units/pd2_dlc_hvh/characters/ene_bulldozer_hvh_2/ene_bulldozer_hvh_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_blackdozer/ene_deathvox_blackdozer",
+--		["units/pd2_dlc_hvh/characters/ene_bulldozer_hvh_3/ene_bulldozer_hvh_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_lmgdozer/ene_deathvox_lmgdozer",
 
--- Security Guards, defer for now. There's a bunch more of these to do! I need to check against the full pool and identify roles.
-		["units/payday2/characters/ene_security_1/ene_security_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_2/ene_security_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_3/ene_security_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_4/ene_security_4"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_5/ene_security_5"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_6/ene_security_6"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_7/ene_security_7"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
-		["units/payday2/characters/ene_security_8/ene_security_8"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard"
+-- Other units.
+	
+-- "Classic" FBI, one of the units players want to see back. We may adjust and sub in a new weaponpreset for them though.
+--		["units/payday2/characters/ene_fbi_1/ene_fbi_1"] = 
+--		["units/payday2/characters/ene_fbi_2/ene_fbi_2"] = 
+--		["units/payday2/characters/ene_fbi_3/ene_fbi_3"] = 
 
--- criminals below here.
+-- Security Guards, not directly replacing. There are  more of these to document! 
+--	I need to check against the full pool and identify roles. Some have pistols, some have smgs, some have shotties.
+--		["units/payday2/characters/ene_security_1/ene_security_1"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_2/ene_security_2"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_3/ene_security_3"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_4/ene_security_4"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_5/ene_security_5"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_6/ene_security_6"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_7/ene_security_7"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard",
+--		["units/payday2/characters/ene_security_8/ene_security_8"] = "units/pd2_mod_gageammo/characters/ene_deathvox_guard/ene_deathvox_guard"
+
+-- Definitely missing Alesso guards here.
+	
+-- Secret Service.
+-- Light.
+-- No light.
+-- Murkywater.
+	
+-- Safehouse guards (Hoxvenge).
+	
+
+-- "Dress" FBI of various sorts, male and female.	
+	
+-- Criminals below here.
 
 -- Commissar's Russian mob in Hotline Miami.
 -- 1:
@@ -111,11 +149,17 @@ local sm_wish = {
 --		["units/payday2/characters/ene_gang_mobster_3/ene_gang_mobster_3"] = 
 -- 4:
 --		["units/payday2/characters/ene_gang_mobster_4/ene_gang_mobster_4"] = 
+-- 5:
+--		["units/payday2/characters/ene_gang_russian_5/ene_gang_russian_5"] =
 
 -- Overkill MC.
 
 -- Cobras.
-
+-- ["units/payday2/characters/ene_gang_black_1/ene_gang_black_1"] =
+-- ["units/payday2/characters/ene_gang_black_2/ene_gang_black_2"] =
+-- ["units/payday2/characters/ene_gang_black_3/ene_gang_black_3"] =
+-- ["units/payday2/characters/ene_gang_black_4/ene_gang_black_4"] =
+	
 -- Mendozas.
 -- 1:
 --		["units/payday2/characters/ene_gang_mexican_1/ene_gang_mexican_1"] = 
@@ -126,11 +170,40 @@ local sm_wish = {
 -- 4:
 --		["units/payday2/characters/ene_gang_mexican_4/ene_gang_mexican_4"] = 
 
--- Russians for Vlad jobs.
+-- Russians for Vlad jobs/other miscellany.
 
 -- Sosa guards.
-
+-- Outdoor 1:
+--		["units/pd2_dlc_friend/characters/ene_bolivian_thug_outdoor_01/ene_bolivian_thug_outdoor_01"] = 
+-- Outdoor 2:
+--		["units/pd2_dlc_friend/characters/ene_bolivian_thug_outdoor_02/ene_bolivian_thug_outdoor_02"] = 
+-- Indoor 1:
+--		["units/pd2_dlc_friend/characters/ene_thug_indoor_01/ene_thug_indoor_01"] = 
+-- Indoor 2:	
+--		["units/pd2_dlc_friend/characters/ene_thug_indoor_02/ene_thug_indoor_02"] = 
+-- Security Manager:
+--		["units/pd2_dlc_friend/characters/ene_security_manager/ene_security_manager"]
+	
+	
 -- Brooklyn 10-10.
+-- Sniper:
+--		["units/pd2_dlc_spa/characters/ene_sniper_3/ene_sniper_3"] = 
+	
+-- Bosses.
+-- Commissar.
+-- Sosa.
+-- Unarmored Sosa.
+-- Biker boss.
+-- Chavez.
+-- Hector.
+-- Unarmored Hector.
+-- Undercover (RvD 1).
+-- FBI Boss (Hoxbreak 2).
+	
+-- Headless TitanDozer.	There might be multiple specified for different heists.
+	
+-- Headless Zealdozer.
+--		["units/pd2_dlc_help/characters/ene_zeal_bulldozer_halloween/ene_zeal_bulldozer_halloween"]	
 	}
 function ElementSpawnEnemyDummy:init(...)
 	ElementSpawnEnemyDummy.super.init(self, ...)
