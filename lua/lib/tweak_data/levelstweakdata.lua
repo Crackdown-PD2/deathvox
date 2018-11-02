73ab4b83dcca940745739891f8f011a685ce5132
iamgoofball <iamgoofball@gmail.com>
2018-11-02 02:40:55 -0700

    breaking news has faded, time to resume work

--- lua/lib/tweak_data/levelstweakdata.lua
+++ lua/lib/tweak_data/levelstweakdata.lua
@@ -1,6 +1,11 @@
-function LevelsTweakData:get_ai_group_type() -- We don't have support for or use the russians/zombies, so we can use this to easily swap visuals for "factions" based on difficulty.
+function LevelsTweakData:get_ai_group_type() -- We can use this to easily swap visuals for "factions" based on difficulty.
 	local group_to_use = "zeal" 			 -- Aka, instead of 1500 difficulty if's to change the group based on what difficulty it is, we can just instead change what faction it's looking for.
 											 -- This makes swapping difficulties on the fly much, much easier, along with maintaining a clean codebase.
+	local level_id
+	if Global.level_data and Global.level_data.level_id then
+		level_data = Global.level_data.level_id
+	end
+	
 	if not Global.game_settings then
 		return group_to_use
 	end
@@ -14,6 +19,7 @@ function LevelsTweakData:get_ai_group_type() -- We don't have support for or use
 		"overkill_290",
 		"sm_wish"
 	}
+	local map_faction_override = {} -- todo: setup akan on BP, murky on all murky heists, and classics on classic heists
 	local diff_index = table.index_of(difficulties, Global.game_settings.difficulty)
 	if diff_index <= 3 then
 		group_to_use = "cop"
@@ -21,6 +27,14 @@ function LevelsTweakData:get_ai_group_type() -- We don't have support for or use
 		group_to_use = "fbi"
 	elseif diff_index <= 7 then
 		group_to_use = "gensec"
+	end
+	if level_id then
+		if map_faction_override[level_id] then
+			group_to_use = map_faction_override[level_id]
+		end
+	end
+	if diff_index == 8 then -- kataru's reach is true
+		group_to_use = "zeal"
 	end
 	return group_to_use
 end
--- lua/loc/loc.lua
+++ lua/loc/loc.lua
@@ -1,6 +1,21 @@
 Hooks:Add("LocalizationManagerPostInit", "DeathVox_Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-		["menu_risk_sm_wish"] = "Serious game for SERIOUS GAMERS.",
-		["menu_difficulty_sm_wish"] = "Crackdown"
+		["menu_risk_sm_wish"] = "Crackdown. A beary hard difficulty.",
+		["hud_assault_cop_assault"] = "POLICE ASSAULT IN PROGRESS",
+		["hud_assault_cop_cover"] = "STAY IN COVER",
+		["hud_assault_fbi_assault"] = "FBI ASSAULT IN PROGRESS",
+		["hud_assault_fbi_cover"] = "STAY IN COVER",
+		["hud_assault_gensec_assault"] = "GenSec Presents: The Assault!",
+		["hud_assault_gensec_cover"] = "Twenty-Four Seven Brand Cover!",
+		["hud_assault_zeal_assault"] = "ZEAL EXTERMINATION UNDERWAY",
+		["hud_assault_zeal_cover"] = "SURRENDER NOW OR FACE THE CONSEQUENCES",
+		["hud_assault_murky_assault"] = "MURKYWATER OPERATION UNDERWAY",
+		["hud_assault_murky_cover"] = "STAY IN COVER",
+		["hud_assault_akan_assault"] = "АКАН НАПАДЕНИЕ В ПРОГРЕСС",
+		["hud_assault_akan_cover"] = "ОСТОРОЖНО",
+		["hud_assault_classic_assault"] = "POLICE ASSAULT IN PROGRESS",
+		["hud_assault_classic_cover"] = "STAY IN COVER",
+		["hud_assault_zombie_assault"] = "ALL HEIST AND NO DRILL",
+		["hud_assault_zombie_cover"] = "MAKES BAIN A DULL SAFE"
 	})
 end)
