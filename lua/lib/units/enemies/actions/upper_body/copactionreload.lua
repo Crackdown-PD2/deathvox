c42cc7c6c714e063965277c9e4ae0beca5ec0ae2
Manuel <44041474+hoxiwoopboop@users.noreply.github.com>
2018-11-11 14:43:12 -0300

    Update copactionreload.lua
    Added a check to prevent a crash if a unit tries to do a looped reload while in bleedout.

--- lua/lib/units/enemies/actions/upper_body/copactionreload.lua
+++ lua/lib/units/enemies/actions/upper_body/copactionreload.lua
@@ -1,8 +1,8 @@
 function CopActionReload:_play_reload(loop_t, loop_t_multiplier)
 	local weap_tweak = self._weapon_unit and self._weapon_unit:base() and self._weapon_unit:base():weapon_tweak_data()
 
-	if weap_tweak and weap_tweak.reload == "looped" then
-		local sound_prefix = weap_tweak.sounds.prefix
+	if loop_t_multiplier and not self._ext_anim.base_no_recoil then --prevents a crash if the unit tries to do a looped reload while in bleedout
+		local sound_prefix = weap_tweak.sounds.prefix --easy way to choose which weapons should be doing "one-round" looped reloads, alternatively something like checking for weap_tweak.single_looped_reload can be done, but would require adding that value in weapontweakdata accordingly
 		local single_reload = sound_prefix == "nagant_npc" or sound_prefix == "ching_npc"
 		local loop_amount = not single_reload and weap_tweak.CLIP_AMMO_MAX or 1
 
@@ -12,7 +12,7 @@ function CopActionReload:_play_reload(loop_t, loop_t_multiplier)
 			self._looped_reload = true
 			self._loop_t = loop_t + (1 * ((0.45 * loop_amount) / loop_t_multiplier))
 		else
-			cat_print("george", "[CopActionReload:_play_reload_looped] redirect failed in", self._machine:segment_state(Idstring("base")))
+			cat_print("george", "[CopActionReload:_play_reload] redirect failed in", self._machine:segment_state(Idstring("base")))
 
 			return
 		end
