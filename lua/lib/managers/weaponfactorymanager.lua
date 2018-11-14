670067bb9fc0a3a58e69934ec13f294aaea9633a
iamgoofball <iamgoofball@gmail.com>
2018-11-14 01:35:39 -0800

    Improves performance of enemy weapons

--- lua/lib/managers/weaponfactorymanager.lua
+++ lua/lib/managers/weaponfactorymanager.lua
@@ -63,3 +63,14 @@ DelayedCalls:Add('DelayedModFSS_getfactoryidbyweaponid', 0, function()
 		return w2f[weapon_id]
 	end
 end)
+
+function WeaponFactoryManager:_spawn_and_link_unit(u_name, a_obj, third_person, link_to_unit)
+	local unit = World:spawn_unit(u_name, Vector3(), Rotation())
+	local res = link_to_unit:link(a_obj, unit, unit:orientation_object():name())
+
+	--if managers.occlusion and not third_person then
+	--	managers.occlusion:remove_occlusion(unit)
+	--end
+
+	return unit
+end
