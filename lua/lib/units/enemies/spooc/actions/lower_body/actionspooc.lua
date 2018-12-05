8e58859b18ed4dabe5511cb39036924d4a1ddb53
DiscendoVox <marriotr@gmail.com>
2018-12-04 22:29:16 -0500

    removes FSS optimization dependency
    removes FSS cloaker movespeed optimization dependency that called a separate FSS movement file.
    This file has been unhooked pending examination; it has optimizations in it that may be viable, but they are mixed with changes that create buggy enemy behaviors. See branch commit info for details of observed behavior issues.

--- lua/lib/units/enemies/spooc/actions/lower_body/actionspooc.lua
+++ lua/lib/units/enemies/spooc/actions/lower_body/actionspooc.lua
@@ -2,11 +2,4 @@ function ActionSpooc:complete()
 	return (self._beating_end_t and self._beating_end_t < TimerManager:game():time()) or (self._beating_end_t and self._last_vel_z >= 0)
 end
 
-local fs_original_actionspooc_init = ActionSpooc.init
-function ActionSpooc:init(action_desc, common_data)
-	local result = fs_original_actionspooc_init(self, action_desc, common_data)
-	self.fs_move_speed = CopActionWalk.fs_move_speeds[common_data.ext_base._tweak_table][self._stance.name][self._haste]
-	return result
-end
-
 ActionSpooc._get_current_max_walk_speed = CopActionWalk._get_current_max_walk_speed
