abff74e1b988cbdb0d84ece8f180b631426cc05f
DiscendoVox <marriotr@gmail.com>
2019-06-21 01:06:39 -0400

    Partial group redefinition, skirmish
    Note this commit is incomplete and will crash or otherwise bork holdout until population of groups is finished.

--- lua/lib/tweak_data/skirmishtweakdata.lua
+++ lua/lib/tweak_data/skirmishtweakdata.lua
@@ -68,7 +68,8 @@ end
 
 function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything about this setup is really nice and I like it a lot. I need to integrate some of this into the core CD groupAI stuff.
 	local nice_human_readable_table = {
-		{ -- wave 1 some tasers, mostly greens/tans + medics
+		{ -- wave 1 beat police
+		  -- Effectively brief warm-up. beat, shield, medic, rookies.
 			2.56, -- 4 heavyar
 			5.55, -- heavyshot/lightar
 			5.55, -- heavyshot/lightar/medic
@@ -89,7 +90,8 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 2 more tasers, less medics
+		{ -- wave 2 swat
+		  -- swat, shield, medic, taser. Add HRT.
 			5.56, -- 4 heavyar
 			5.55, -- heavyshot/lightar
 			3.55, -- heavyshot/lightar/medic
@@ -110,7 +112,8 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 3 introduce LMG dozer groups
+		{ -- wave 3 FBI
+		  -- warmup over. FBI, medics, shields, dozers, cloakers, HRT.
 			5.56, -- 4 heavyar
 			3.55, -- heavyshot/lightar
 			3.55, -- heavyshot/lightar/medic
@@ -131,7 +134,8 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 4 no change
+		{ -- wave 4 Gensec. Mayhem values.
+		  -- Gensecs, dozers (w/LMG), cloakers, medics, shields. Add Veterans.
 			5.56, -- 4 heavyar
 			3.55, -- heavyshot/lightar
 			3.55, -- heavyshot/lightar/medic
@@ -152,7 +156,8 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 5 introduce more shotgunners, less rifles
+		{ -- wave 5 Classic. Death Wish values.
+		  -- extensive semi-endurance. Full range of units including earlier, e.g. beat police, HRT.
 			2.56, -- 4 heavyar
 			3.55, -- heavyshot/lightar
 			3.55, -- heavyshot/lightar/medic
@@ -173,7 +178,8 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 6 no change
+		{ -- wave 6 Zeal.
+		  -- shorter than wave 5. Full assault range except grenadiers. Add medicdozers.
 			2.56, -- 4 heavyar
 			3.55, -- heavyshot/lightar
 			3.55, -- heavyshot/lightar/medic
@@ -194,7 +200,7 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			0, --grenadier/cloaker/taser
 			0 --shield/grenadier
 		},
-		{ -- wave 7 introduce medicdozers
+		{ -- wave 7 ???
 			3.56, -- 4 heavyar
 			3.55, -- heavyshot/lightar
 			2.55, -- heavyshot/lightar/medic
@@ -258,26 +264,67 @@ function SkirmishTweakData:_init_spawn_group_weights(tweak_data)  -- Everything 
 			4 --shield/grenadier
 		}
 	}
-	local ordered_spawn_group_names = {
-		"cd_group_1",
-		"cd_group_2_std ",
-		"cd_group_2_med ",
-		"cd_group_3_std",
-		"dv_group_3_med",
-		"cd_group_4_std",
-		"cd_group_4_med",
-		"cd_group_5_std",
-		"cd_group_5_med",
-		"gorgon",
-		"atlas",
-		"chimera",
-		"zeus",
-		"janus",
-		"epeius",
-		"damocles",
-		"caduceus",
-		"atropos",
-		"aegeas"
+	local ordered_spawn_group_names = { 
+	-- aim for 10 or fewer groups per aside from wave 5 classics.
+	-- Beat beat, shield, medic, rookies.
+		"normal_rookiepair", -- #1A two rookie
+		"normal_revolvergroup", -- #1B revolver/two pistol
+		"hard_smggroup", -- #1C smg/two pistol
+		"hard_copcombo", -- #1D shot/two smg
+		"hard_revolvermedic", -- #1E revolver/medic
+		"hard_rookiemedic", -- #1F rookie/medic
+		"normal_shieldbasic", -- #1G shield/shot
+		
+	-- SWAT swat, shield, medic, taser. Add HRT.
+		"hard_lightgroup", -- #2A one Lshot/3 LAR
+		"hard_shieldheavy", -- #2B one Shield/2 HAR
+		"vhard_taserARgroup", -- #2C one Taser/2 HAR
+		"vhard_tasershotgroup", -- #2D one Taser/2 Hshot
+		"vhard_shieldlightgroup", -- #2E one Shield/2 LAR
+		"vhard_lightARchargeMed", -- #2F three LAR/1 Medic
+		"vhard_hrtmedic", -- #2G 2 HRT/1 Medic
+		"vhard_hrttaser", -- #2H one Taser/2 HRT
+
+	-- FBI warmup over. FBI, medics, shields, dozers, cloakers, HRT.
+		"ovk_mixheavyARcharge", -- #3A two HAR/2 LAR
+		"ovk_comboARcharge", -- #3B two HAR/1 Medic/1 LAR
+		"ovk_mixshotcharge", -- #3C two Hshot/2 Lshot
+		"ovk_shieldcombo", -- #3D two Shield/1 Medic/ 1 Lshot
+		"ovk_shieldshot", -- #3E two Shield/2 Lshot
+		"ovk_tazerAR", -- #3F two Taser/2 LAR
+		"ovk_greenknight" -- #3G one Shield/1 Gdozer
+		"ovk_blackknight", -- #3H one Shield/1 Bdozer
+		"ovk_spoocpair", -- #3I	two cloaker
+		"vhard_hrtmedic", -- #3J two HRT/1 Medic
+
+	-- Gensec Gensecs, dozers (w/LMG), cloakers, medics, shields. Add Veterans.
+		"mh_group_4_std", -- #4A one Hshot/2 Lshot/1 LAR
+		"mh_group_4_med", -- #4B one Hshot/2 Lshot/1 Medic
+		"mh_group_5_std", -- #4C two HAR/2 LAR
+		"mh_group_5_med", -- #4D two HAR/1 LAR/1 Medic
+		"mh_whitedozer", -- #4E one Wdozer
+		"mh_blackpaladin", -- #4F one shield/1 Bdozer/1 Medic
+		"mh_greenpaladin", -- #4G  one shield/1 Gdozer/1 Medic
+		"mh_partners", -- #4H one Gdozer/1 Bdozer
+		"mh_taserpair", -- #4I  two tasers
+		"mh_takedown", -- #4J  one taser/one cloaker
+		"mh_spoocpair", -- #4K  two cloakers
+		"mh_vetmed", -- #4L  two veterans/2 Medics
+		
+	-- Classic extensive semi-endurance. Full range of units including earlier, e.g. beat police, HRT.
+		
+	-- Zeal shorter than wave 5. Full assault range except grenadiers. Add medicdozers.
+		"cd_group_2_std", -- #6A two Hshot/2 LAR
+		"cd_group_4_std", -- #6B one Hshot/2 Lshot/1 LAR
+		"cd_group_5_std", -- #6C two HAR/2 LAR
+		"cd_group_5_med", -- #6D two HAR/1 LAR/1 Medic
+		"gorgon", -- #6E two taser/1 Medic
+		"chimera", -- #6F one tazer/1 Bdozer/1 Medic/1 cloaker
+		"janus", -- #6G one Bdozer/1 Medicdozer
+		"mh_greenpaladin", -- #6H one Shield/1 Gdozer/1 Medic
+		"epeius", -- #6I three shield/1 Wdozer
+		"damocles", -- #6J three cloakers
+		"dw_vetmed" -- #6K two veterans/2 Medic
 	}
 
 	for _, wave in ipairs(nice_human_readable_table) do
