9cfc5d664efdf437a384be317a53554c8c343c65
Manuel <44041474+hoxiwoopboop@users.noreply.github.com>
2019-08-09 00:18:08 -0300

    Added pump-action for looped reloads when needed
    Missing the reload SFX though, I've been trying to find it to no avail.

--- lua/lib/units/enemies/actions/upper_body/copactionreload.lua
+++ lua/lib/units/enemies/actions/upper_body/copactionreload.lua
@@ -1,16 +1,19 @@
-function CopActionReload:_play_reload(loop_t, loop_t_multiplier)
+function CopActionReload:_play_reload(t, loop_t_multiplier)
 	local weap_tweak = self._weapon_unit and self._weapon_unit:base() and self._weapon_unit:base():weapon_tweak_data()
 
-	if loop_t_multiplier and not self._ext_anim.base_no_recoil then --prevents a crash if the unit tries to do a looped reload while in bleedout
-		local sound_prefix = weap_tweak.sounds.prefix --easy way to choose which weapons should be doing "one-round" looped reloads, alternatively something like checking for weap_tweak.single_looped_reload can be done, but would require adding that value in weapontweakdata accordingly
-		local single_reload = sound_prefix == "nagant_npc" or sound_prefix == "ching_npc"
-		local loop_amount = not single_reload and weap_tweak.CLIP_AMMO_MAX or 1
+	if loop_t_multiplier and not self._ext_anim.base_no_recoil then
+		local sound_prefix = weap_tweak.sounds.prefix --using weapon sounds because vanilla friendly, customize as you wish
+		local single_reload = sound_prefix == "nagant_npc" or sound_prefix == "ching_npc" or sound_prefix == "ecp_npc"
+		local use_pump_anim_on_end = sound_prefix == "remington_npc" or sound_prefix == "serbu_npc" or sound_prefix == "keltec_npc" or sound_prefix == "m37_npc" or sound_prefix == "china_npc"
+		local loop_amount = not single_reload and (use_pump_anim_on_end and weap_tweak.CLIP_AMMO_MAX - 3 or weap_tweak.CLIP_AMMO_MAX) or 1
 
 		local redir_res = self._ext_movement:play_redirect("reload_looped")
 
 		if redir_res then
 			self._looped_reload = true
-			self._loop_t = loop_t + (1 * ((0.45 * loop_amount) / loop_t_multiplier))
+			self._loop_t = t + (1 * ((0.45 * loop_amount) / loop_t_multiplier))
+			self._loop_t_multiplier = loop_t_multiplier
+			self._use_pump = use_pump_anim_on_end
 		else
 			cat_print("george", "[CopActionReload:_play_reload] redirect failed in", self._machine:segment_state(Idstring("base")))
 
@@ -34,9 +37,18 @@ end
 function CopActionReload:update_looped(t)
 	if self._looped_reload then
 		if self._loop_t < t then
-			local res = self._ext_movement:play_redirect("reload_looped_exit")
+			local res = self._use_pump and self._ext_movement:play_redirect("reload") or self._ext_movement:play_redirect("reload_looped_exit")
 
 			if res then
+				if self._use_pump then --sadly, the shotgun reload + pump is missing the sfx for now, and I've been trying to locate it to no avail, would be thankful if someone knows about this
+					if self._loop_t_multiplier then
+						self._machine:set_speed(res, self._loop_t_multiplier)
+						self._loop_t_multiplier = nil
+					end
+
+					self._use_pump = nil
+				end
+
 				self._looped_reload = nil
 			end
 		end
