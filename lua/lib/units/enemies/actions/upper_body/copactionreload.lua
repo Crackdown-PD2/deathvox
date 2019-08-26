2365d93a3a607562872c2288f55749aa724991b5
Manuel <44041474+hoxiwoopboop@users.noreply.github.com>
2019-08-25 23:42:31 -0300

    Removed pump animation on end
    It has no sounds and looks weird on Dozers.

--- lua/lib/units/enemies/actions/upper_body/copactionreload.lua
+++ lua/lib/units/enemies/actions/upper_body/copactionreload.lua
@@ -4,8 +4,7 @@ function CopActionReload:_play_reload(t, loop_t_multiplier)
 	if loop_t_multiplier and not self._ext_anim.base_no_recoil then
 		local sound_prefix = weap_tweak.sounds.prefix --using weapon sounds because vanilla friendly, customize as you wish
 		local single_reload = sound_prefix == "nagant_npc" or sound_prefix == "ching_npc" or sound_prefix == "ecp_npc"
-		local use_pump_anim_on_end = sound_prefix == "remington_npc" or sound_prefix == "serbu_npc" or sound_prefix == "keltec_npc" or sound_prefix == "m37_npc" or sound_prefix == "china_npc"
-		local loop_amount = not single_reload and (use_pump_anim_on_end and weap_tweak.CLIP_AMMO_MAX - 3 or weap_tweak.CLIP_AMMO_MAX) or 1
+		local loop_amount = not single_reload and weap_tweak.CLIP_AMMO_MAX or 1
 
 		local redir_res = self._ext_movement:play_redirect("reload_looped")
 
@@ -13,7 +12,6 @@ function CopActionReload:_play_reload(t, loop_t_multiplier)
 			self._looped_reload = true
 			self._loop_t = t + (1 * ((0.45 * loop_amount) / loop_t_multiplier))
 			self._loop_t_multiplier = loop_t_multiplier
-			self._use_pump = use_pump_anim_on_end
 		else
 			cat_print("george", "[CopActionReload:_play_reload] redirect failed in", self._machine:segment_state(Idstring("base")))
 
@@ -37,18 +35,9 @@ end
 function CopActionReload:update_looped(t)
 	if self._looped_reload then
 		if self._loop_t < t then
-			local res = self._use_pump and self._ext_movement:play_redirect("reload") or self._ext_movement:play_redirect("reload_looped_exit")
+			local res = self._ext_movement:play_redirect("reload_looped_exit")
 
 			if res then
-				if self._use_pump then --sadly, the shotgun reload + pump is missing the sfx for now, and I've been trying to locate it to no avail, would be thankful if someone knows about this
-					if self._loop_t_multiplier then
-						self._machine:set_speed(res, self._loop_t_multiplier)
-						self._loop_t_multiplier = nil
-					end
-
-					self._use_pump = nil
-				end
-
 				self._looped_reload = nil
 			end
 		end
