5e78a8fd14ce2e6eb97cd21674eb61810914359b
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2019-10-28 00:11:49 -0300

    Forgot to delete this when I made 1-ray shots

--- lua/lib/units/equipment/sentry_gun/sentrygunbase.lua
+++
@@ -1,3 +0,0 @@
-function SentryGunBase:_fire_raycast(user_unit, from_pos, direction, shoot_player, target_unit, shoot_through_data)
-	return self._unit:weapon():_fire_raycast(user_unit, from_pos, direction, shoot_player, target_unit, shoot_through_data)
-end
