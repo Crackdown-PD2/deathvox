e1cce104ca7c50410f4114ad551656184c9727aa
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-01-05 16:43:13 -0300

    weapontweakdata stat entry template part 3

--- lua/lib/tweak_data/weapontweakdata.lua
+++ lua/lib/tweak_data/weapontweakdata.lua
@@ -1427,7 +1427,7 @@ end
 
 Hooks:PostHook(WeaponTweakData, "_init_data_player_weapons", "vox_wep", function(self, tweak_data)
 	
-	local dont = true
+	--local dont = true
 	
 	if dont then
 		--Don't worry about all of this.
