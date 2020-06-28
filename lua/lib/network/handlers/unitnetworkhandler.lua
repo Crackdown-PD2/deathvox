5da9855354d8d9c553d12f1f3fc9e7d67e573d18
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-28 20:19:43 -0300

    Updated how some things are synced
    Includes:
    
    -Proper Medic healing syncing (also done through other files, not just this one). This also includes actually applying the heal cooldown across peers.
    
    -Proper hurt animation syncing (also done through other files) without having to tweak so many network functions, while also doing it more accurately.
    
    -Handling corpse body parts syncing through here (although unused atm).

--- lua/lib/network/handlers/unitnetworkhandler.lua
+++ lua/lib/network/handlers/unitnetworkhandler.lua
@@ -13,7 +13,7 @@ function UnitNetworkHandler:sync_friendly_fire_damage(peer_id, unit, damage, var
 				damage = damage,
 				variant = variant,
 				col_ray = {
-					position = unit:movement():m_head_pos()
+					position = mvector3.copy(unit:movement():m_head_pos())
 				}
 			}
 
@@ -24,8 +24,7 @@ function UnitNetworkHandler:sync_friendly_fire_damage(peer_id, unit, damage, var
 					attack_info.tase_player = true --allow melee tase to do a non-lethal tase against other players
 				end
 
-				local push_vec = Vector3()
-				mvector3.direction(push_vec, unit:movement():m_head_pos(), managers.player:player_unit():movement():m_head_pos())
+				local push_vec = player_unit:movement():m_head_pos() - unit:movement():m_head_pos()
 
 				attack_info.push_vel = push_vec:with_z(0.1):normalized() * 600
 
@@ -89,7 +88,7 @@ function UnitNetworkHandler:action_aim_state(unit, state)
 end
 
 function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike, action_id)
-	if not self._verify_character(unit) or not self._verify_gamestate(self._gamestate_filter.any_ingame) then
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character(unit) then
 		return
 	end
 
@@ -105,6 +104,7 @@ function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike
 		flying_strike = flying_strike,
 		action_id = action_id,
 		blocks = {
+			idle = -1,
 			act = -1,
 			turn = -1,
 			walk = -1
@@ -112,7 +112,6 @@ function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike
 	}
 
 	if flying_strike then
-		action_desc.blocks.idle = -1
 		action_desc.blocks.light_hurt = -1
 		action_desc.blocks.heavy_hurt = -1
 		action_desc.blocks.fire_hurt = -1
@@ -122,4 +121,199 @@ function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike
 	end
 
 	unit:movement():action_request(action_desc)
+end
+
+function UnitNetworkHandler:action_hurt_start(unit, hurt_type_idx, body_part, death_type_idx, type_idx, variant_idx, direction_vec, hit_pos)
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) then
+		return
+	end
+
+	local hurt_type = CopActionHurt.idx_to_hurt_type(hurt_type_idx)
+
+	if hurt_type == "death" then
+		if alive(unit) then
+			local action_type = CopActionHurt.idx_to_type(type_idx)
+			local variant = CopActionHurt.idx_to_variant(variant_idx)
+			local block_type = hurt_type
+			local mov_ext = unit:movement()
+			local client_interrupt = nil
+
+			if mov_ext._queued_actions then
+				mov_ext._queued_actions = {}
+			end
+
+			if mov_ext._rope then
+				mov_ext._rope:base():retract()
+
+				mov_ext._rope = nil
+				mov_ext._rope_death = true
+
+				if unit:sound().anim_clbk_play_sound then
+					unit:sound():anim_clbk_play_sound(unit, "repel_end")
+				end
+			end
+
+			if Network:is_server() then
+				mov_ext:set_attention()
+			else
+				client_interrupt = true
+				mov_ext:synch_attention()
+			end
+
+			local blocks = {
+				act = -1,
+				aim = -1,
+				action = -1,
+				tase = -1,
+				walk = -1,
+				light_hurt = -1,
+				death = -1
+			}
+
+			local action_data = {
+				allow_network = false,
+				client_interrupt = client_interrupt,
+				hurt_type = hurt_type,
+				block_type = block_type,
+				blocks = blocks,
+				body_part = body_part,
+				death_type = CopActionHurt.idx_to_death_type(death_type_idx),
+				type = action_type,
+				variant = variant,
+				direction_vec = direction_vec,
+				hit_pos = hit_pos
+			}
+
+			unit:movement():action_request(action_data)
+		end
+	else
+		if not self._verify_character(unit) then
+			return
+		end
+
+		local action_data = nil
+		local action_type = CopActionHurt.idx_to_type(type_idx)
+
+		if action_type == "healed" then
+			if unit:anim_data() and unit:anim_data().act then
+				return
+			end
+
+			action_data = {
+				body_part = body_part,
+				type = "healed",
+				client_interrupt = Network:is_client()
+			}
+		else
+			local block_type = hurt_type
+
+			if hurt_type == "expl_hurt" or hurt_type == "fire_hurt" or hurt_type == "poison_hurt" or hurt_type == "taser_tased" then
+				block_type = "heavy_hurt"
+			end
+
+			if Network:is_server() and unit:movement():chk_action_forbidden(block_type) then
+				return
+			end
+
+			local variant = CopActionHurt.idx_to_variant(variant_idx)
+			local client_interrupt, blocks = nil
+
+			if variant == "tase" then
+				block_type = "bleedout"
+			elseif hurt_type == "expl_hurt" or hurt_type == "fire_hurt" or hurt_type == "poison_hurt" or hurt_type == "taser_tased" then
+				block_type = "heavy_hurt"
+
+				client_interrupt = Network:is_client()
+			else
+				block_type = hurt_type
+
+				if hurt_type ~= "bleedout" and hurt_type ~= "fatal" then
+					client_interrupt = Network:is_client()
+				end
+			end
+
+			if hurt_type ~= "light_hurt" then
+				blocks = {
+					act = -1,
+					aim = -1,
+					action = -1,
+					tase = -1,
+					walk = -1,
+					light_hurt = -1
+				}
+
+				if hurt_type == "bleedout" then
+					blocks.bleedout = -1
+					blocks.hurt = -1
+					blocks.heavy_hurt = -1
+					blocks.hurt_sick = -1
+					blocks.concussion = -1
+				end
+			end
+
+			action_data = {
+				allow_network = false,
+				client_interrupt = client_interrupt,
+				hurt_type = hurt_type,
+				block_type = block_type,
+				blocks = blocks,
+				body_part = body_part,
+				death_type = CopActionHurt.idx_to_death_type(death_type_idx),
+				type = action_type,
+				variant = variant,
+				direction_vec = direction_vec,
+				hit_pos = hit_pos
+			}
+		end
+
+		unit:movement():action_request(action_data)
+	end
+end
+
+function UnitNetworkHandler:sync_medic_heal(unit, sender)
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_sender(sender) then
+		return
+	end
+
+	MedicActionHeal:check_achievements()
+
+	if self._verify_character(unit) then
+		unit:character_damage()._heal_cooldown_t = Application:time()
+
+		if unit:anim_data() and unit:anim_data().act then
+			unit:sound():say("heal")
+		else
+			local action_data = {
+				body_part = 1,
+				type = "heal",
+				client_interrupt = Network:is_client()
+			}
+
+			unit:movement():action_request(action_data)
+		end
+	end
+end
+
+function UnitNetworkHandler:m79grenade_explode_on_client(position, normal, user, damage, range, curve_pow, sender)
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) then
+		return
+	end
+
+	if not self._verify_character_and_sender(user, sender) then
+		if alive(user) and user:movement() and user:movement._active_actions then --this instance was actually a shotgun push ragdoll pos sync
+			local death_action = user:movement._active_actions[1]
+
+			if death_action and death_action:type() == "hurt" and death_action._hips_obj then
+				local u_body = user:body(damage)
+
+				if u_body:enabled() and u_body:dynamic() then
+					u_body:set_position(position)
+				end
+			end
+		end
+
+		return
+	end
+
+	ProjectileBase._explode_on_client(position, normal, user, damage, range, curve_pow)
 end
