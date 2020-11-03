11d4cb3215552fc227da532a017f329e3194d8c9
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-11-02 21:29:22 -0300

    Hoppip's grace period fix is now enabled by default

--- lua/lib/units/beings/player/playerdamage.lua
+++ lua/lib/units/beings/player/playerdamage.lua
@@ -207,19 +207,22 @@ function PlayerDamage:clbk_kill_taunt_common(attack_data)
 	self._kill_taunt_clbk_id = nil
 end
 
-local _chk_dmg_too_soon_orig = PlayerDamage._chk_dmg_too_soon
+--local _chk_dmg_too_soon_orig = PlayerDamage._chk_dmg_too_soon
 function PlayerDamage:_chk_dmg_too_soon(damage, ...)
-	if not deathvox:IsHoppipOverhaulEnabled() then
+	--[[if not deathvox:IsHoppipOverhaulEnabled() then
 		return _chk_dmg_too_soon_orig(self, damage, ...)
-   	end
-	
+   	end]]
+
 	local next_allowed_dmg_t = type(self._next_allowed_dmg_t) == "number" and self._next_allowed_dmg_t or Application:digest_value(self._next_allowed_dmg_t, false)
 	local t = managers.player:player_timer():time()
+
 	if damage <= self._last_received_dmg + 0.01 and next_allowed_dmg_t > t then
 		self._old_last_received_dmg = nil
 		self._old_next_allowed_dmg_t = nil
+
 		return true
 	end
+
 	if next_allowed_dmg_t > t then
 		self._old_last_received_dmg = self._last_received_dmg
 		self._old_next_allowed_dmg_t = next_allowed_dmg_t
@@ -228,29 +231,35 @@ end
 
 local _calc_armor_damage_original = PlayerDamage._calc_armor_damage
 function PlayerDamage:_calc_armor_damage(attack_data, ...)
-
-	if not deathvox:IsHoppipOverhaulEnabled() then
+	--[[if not deathvox:IsHoppipOverhaulEnabled() then
 		return _calc_armor_damage_original(self, attack_data, ...)
-   	end
-	
-	attack_data.damage = attack_data.damage - (self._old_last_received_dmg or 0)
+   	end]]
+
+	if self._old_last_received_dmg then
+		attack_data.damage = attack_data.damage - self._old_last_received_dmg
+	end
+
 	self._next_allowed_dmg_t = self._old_next_allowed_dmg_t and Application:digest_value(self._old_next_allowed_dmg_t, true) or self._next_allowed_dmg_t
 	self._old_last_received_dmg = nil
 	self._old_next_allowed_dmg_t = nil
+
 	return _calc_armor_damage_original(self, attack_data, ...)
 end
 
 local _calc_health_damage_original = PlayerDamage._calc_health_damage
 function PlayerDamage:_calc_health_damage(attack_data, ...)
-
-	if not deathvox:IsHoppipOverhaulEnabled() then
+	--[[if not deathvox:IsHoppipOverhaulEnabled() then
 		return _calc_health_damage_original(self, attack_data, ...)
-   	end
-	
-	attack_data.damage = attack_data.damage - (self._old_last_received_dmg or 0)
+   	end]]
+
+	if self._old_last_received_dmg then
+		attack_data.damage = attack_data.damage - self._old_last_received_dmg
+	end
+
 	self._next_allowed_dmg_t = self._old_next_allowed_dmg_t and Application:digest_value(self._old_next_allowed_dmg_t, true) or self._next_allowed_dmg_t
 	self._old_last_received_dmg = nil
 	self._old_next_allowed_dmg_t = nil
+
 	return _calc_health_damage_original(self, attack_data, ...)
 end
 
