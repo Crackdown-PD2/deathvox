fd5bc1f71ac1af5fda18355627f0796c7df37eb3
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-11-15 21:20:13 -0300

    Post-hook Grenadier's projectile (thanks Fug!)

--- lua/lib/tweak_data/blackmarket/projectilestweakdata.lua
+++ lua/lib/tweak_data/blackmarket/projectilestweakdata.lua
@@ -1,7 +1,6 @@
-local old_init_projectiles = BlackMarketTweakData._init_projectiles
-function BlackMarketTweakData:_init_projectiles(tweak_data)
-	old_init_projectiles(self, tweak_data)
+Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "cdgren", function(self, tweak_data)
 	self.projectiles.dv_grenadier_grenade = deep_clone(self.projectiles.launcher_frag)
 	self.projectiles.dv_grenadier_grenade.unit = "units/pd2_mod_gageammo/pew_pew_lasers/wpn_deathvox_grenadier_grenade"
+
 	table.insert(self._projectiles_index, "dv_grenadier_grenade")
-end
+end)
