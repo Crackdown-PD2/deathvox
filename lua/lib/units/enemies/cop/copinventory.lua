93a09d032d73973f2688d837107876adfc269e9b
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-12-05 02:54:42 -0300

    Turns out the old CopInventory code was used
    And the unwanted comma wasn't nuking the file somehow. I also went ahead and polished said code in the process.

--- lua/lib/units/enemies/cop/copinventory.lua
+++ lua/lib/units/enemies/cop/copinventory.lua
@@ -1,3 +1,18 @@
+local mvec3_set = mvector3.set
+local mvec3_mul = mvector3.multiply
+
+local tmp_vec1 = Vector3()
+
+local math_random = math.random
+
+local idstr_func = Idstring
+local unit_idstr = idstr_func("unit")
+local medium_col_unit_idstr = idstr_func("units/payday2/weapons/box_collision/box_collision_medium_ar")
+local medium_col_idstr = idstr_func("rp_box_collision_medium")
+
+local world_g = World
+local to_number_g = tonumber
+
 function CopInventory:drop_weapon()
 	local selection = self._available_selections[self._equipped_selection]
 	local unit = selection and selection.unit
@@ -9,8 +24,23 @@ function CopInventory:drop_weapon()
 
 		if unit:damage() then
 			unit:damage():run_sequence_simple("enable_body")
-			managers.game_play_central:weapon_dropped(unit)
+		else
+			local u_pos = unit:position()
+			local u_rot = unit:rotation()
+			local dropped_col = world_g:spawn_unit(medium_col_unit_idstr, u_pos, u_rot)
+
+			if dropped_col then
+				dropped_col:link(medium_col_idstr, unit)
+				unit:base()._collider_unit = dropped_col
+
+				mvec3_set(tmp_vec1, u_rot:y())
+				mvec3_mul(tmp_vec1, math_random(75, 200))
+
+				dropped_col:push(10, tmp_vec1)
+			end
 		end
+
+		managers.game_play_central:weapon_dropped(unit)
 
 		local second_gun = unit:base() and alive(unit:base()._second_gun) and unit:base()._second_gun
 
@@ -19,33 +49,49 @@ function CopInventory:drop_weapon()
 
 			if second_gun:damage() then
 				second_gun:damage():run_sequence_simple("enable_body")
-				managers.game_play_central:weapon_dropped(second_gun)
+			else
+				local u_pos = second_gun:position()
+				local u_rot = second_gun:rotation()
+				local dropped_col = world_g:spawn_unit(medium_col_unit_idstr, u_pos, u_rot)
+
+				if dropped_col then
+					dropped_col:link(medium_col_idstr, second_gun)
+					second_gun:base()._collider_unit = dropped_col
+
+					mvec3_set(tmp_vec1, u_rot:y())
+					mvec3_mul(tmp_vec1, math_random(75, 200))
+
+					dropped_col:push(10, tmp_vec1)
+				end
 			end
+
+			managers.game_play_central:weapon_dropped(second_gun)
 		end
 
 		self:_call_listeners("unequip")
 	end
 end
 
---[[function CopInventory:add_unit_by_factory_name(factory_name, equip, instant, blueprint_string, cosmetics_string)
+function CopInventory:add_unit_by_factory_name(factory_name, equip, instant, blueprint_string, cosmetics_string)
 	local factory_weapon = tweak_data.weapon.factory[factory_name]
-	local ids_unit_name = Idstring(factory_weapon.unit)
+	local ids_unit_name = idstr_func(factory_weapon.unit)
+	local dyn_rsr_manager = managers.dyn_resource
 
-	if not managers.dyn_resource:is_resource_ready(Idstring("unit"), ids_unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE) then
-		managers.dyn_resource:load(Idstring("unit"), ids_unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE, nil)
+	if not dyn_rsr_manager:is_resource_ready(unit_idstr, ids_unit_name, dyn_rsr_manager.DYN_RESOURCES_PACKAGE) then
+		dyn_rsr_manager:load(unit_idstr, ids_unit_name, dyn_rsr_manager.DYN_RESOURCES_PACKAGE, nil)
 	end
 
-	local blueprint = nil
-	blueprint = blueprint_string and blueprint_string ~= "" and managers.weapon_factory:unpack_blueprint_from_string(factory_name, blueprint_string) or managers.weapon_factory:get_default_blueprint_by_factory_id(factory_name)
-	local cosmetics = nil
+	local blueprint = blueprint_string and blueprint_string ~= "" and managers.weapon_factory:unpack_blueprint_from_string(factory_name, blueprint_string) or managers.weapon_factory:get_default_blueprint_by_factory_id(factory_name)
 	local cosmetics_data = string.split(cosmetics_string, "-")
 	local weapon_skin_id = cosmetics_data[1] or "nil"
-	local quality_index_s = cosmetics_data[2] or "1"
-	local bonus_id_s = cosmetics_data[3] or "0"
+	local cosmetics = nil
 
 	if weapon_skin_id ~= "nil" then
-		local quality = tweak_data.economy:get_entry_from_index("qualities", tonumber(quality_index_s))
+		local quality_index_s = cosmetics_data[2] or "1"
+		local bonus_id_s = cosmetics_data[3] or "0"
 		local bonus = bonus_id_s == "1" and true or false
+		local quality = tweak_data.economy:get_entry_from_index("qualities", to_number_g(quality_index_s))
+
 		cosmetics = {
 			id = weapon_skin_id,
 			quality = quality,
@@ -53,44 +99,34 @@ end
 		}
 	end
 
-	self:add_unit_by_factory_blueprint(factory_name, equip, instant, blueprint, cosmetics)
-end
+	local new_unit = world_g:spawn_unit(ids_unit_name, Vector3(), Rotation())
 
-function CopInventory:add_unit_by_factory_blueprint(factory_name, equip, instant, blueprint, cosmetics)
-	local factory_weapon = tweak_data.weapon.factory[factory_name]
-	local ids_unit_name = Idstring(factory_weapon.unit)
-
-	if not managers.dyn_resource:is_resource_ready(Idstring("unit"), ids_unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE) then
-		managers.dyn_resource:load(Idstring("unit"), ids_unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE, nil)
+	if not new_unit then
+		return --oh no
 	end
-
-	local new_unit = World:spawn_unit(Idstring(factory_weapon.unit), Vector3(), Rotation())
 
 	new_unit:base():set_factory_data(factory_name)
 	new_unit:base():set_cosmetics_data(cosmetics)
 	new_unit:base():assemble_from_blueprint(factory_name, blueprint)
 	new_unit:base():check_npc()
-	
-	local setup_data = {
-		user_unit = nil,
-		ignore_units = {},
-		expend_ammo = nil,
-		hit_player = nil,
-		user_sound_variant = nil,
-		alert_AI = nil,
-		alert_filter = nil
-	}
+
+	managers.mutators:modify_value("CopInventory:add_unit_by_name", self)
+	self:_chk_spawn_shield(new_unit)
+
+	local setup_data = nil
+
 	if Network:is_server() then
 		setup_data = {
 			user_unit = self._unit,
 			ignore_units = {
 				self._unit,
-				new_unit
+				new_unit,
+				self._shield_unit
 			},
 			expend_ammo = false,
-			autoaim = false,
-			user_sound_variant = "1",
+			hit_slotmask = managers.slot:get_mask("bullet_impact_targets"),
 			hit_player = true,
+			user_sound_variant = "1",
 			alert_AI = true,
 			alert_filter = self._unit:brain():SO_access()
 		}
@@ -99,54 +135,21 @@ function CopInventory:add_unit_by_factory_blueprint(factory_name, equip, instant
 			user_unit = self._unit,
 			ignore_units = {
 				self._unit,
-				new_unit
+				new_unit,
+				self._shield_unit
 			},
 			expend_ammo = false,
-			autoaim = false,
-			user_sound_variant = "1",
+			hit_slotmask = managers.slot:get_mask("bullet_impact_targets_no_AI"),
 			hit_player = true,
-			alert_AI = false
+			user_sound_variant = "1"
 		}
 	end
 
 	new_unit:base():setup(setup_data)
-	self:add_unit(new_unit, equip, instant)
 
 	if new_unit:base().AKIMBO then
 		new_unit:base():create_second_gun()
 	end
+
+	self:add_unit(new_unit, equip, instant)
 end
-
-local temp_vec1 = Vector3()
-local drop_weapon_original = CopInventory.drop_weapon
-function CopInventory:drop_weapon(...)
-  local selection = self._available_selections[self._equipped_selection]
-  local unit = selection and selection.unit
-
-  if unit and unit:damage() then
-    return drop_weapon_original(self, ...)
-  end
-
-  local create_physics_body = function (unit, right)
-    local dropped_col = World:spawn_unit(Idstring("units/payday2/weapons/box_collision/box_collision_medium_ar"), unit:position(), unit:rotation())
-    dropped_col:link(Idstring("rp_box_collision_medium"), unit)
-    mvector3.set(temp_vec1, unit:rotation():y())
-    mvector3.multiply(temp_vec1, math.random(75, 200))
-    dropped_col:push(10, temp_vec1)
-    unit:base()._collider_unit = dropped_col
-  end
-
-  if unit then
-    unit:unlink()
-    create_physics_body(unit)
-    self:_call_listeners("unequip")
-    managers.game_play_central:weapon_dropped(unit)
-
-    if unit:base() and unit:base()._second_gun then
-      local second_gun = unit:base()._second_gun
-      second_gun:unlink()
-      create_physics_body(unit, true)
-      managers.game_play_central:weapon_dropped(second_gun)
-    end
-  end
-end]]
