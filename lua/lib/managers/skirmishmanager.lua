7532aaaebbbd5a364223e3a07251f18419f2e8b0
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-12-07 04:55:52 -0300

    Fixed Holdout not granting any EXP for clients
    The issue was related to how the difficulty change was synced (which wasn't going to work properly when reaching CD difficulty anyway, due to the network function clamping the difficulty index to 8). So, instead, we'll make it happen locally for all peers.
    
    Also made it so players dropping into a Holdout in progress get the difficulty change applied properly as well.

--- lua/lib/managers/skirmishmanager.lua
+++ lua/lib/managers/skirmishmanager.lua
@@ -1,49 +1,52 @@
-local on_start_assault_original = SkirmishManager.on_start_assault
-function SkirmishManager:on_start_assault()
-	if not self._set_first_assault then
-		local diff = "normal"
-		local job_id_index = tweak_data.narrative:get_index_from_job_id(managers.job:current_job_id())
-		local level_id_index = tweak_data.levels:get_index_from_level_id(Global.game_settings.level_id)
-		local difficulty_index = tweak_data:difficulty_to_index(diff)
-		local one_down = Global.game_settings.one_down and true or false		
-		Global.game_settings.difficulty = diff
-		tweak_data:set_difficulty(diff)
+local wave_difficulties = {
+	"normal", --1
+	"hard", --2
+	"overkill", --3
+	"overkill_145", --4
+	"overkill_145", --5
+	"easy_wish", --6
+	"overkill_290", --7
+	"overkill_290", --8
+	"sm_wish" --9
+}
 
-		if managers.network then
-			managers.network:session():send_to_peers("sync_game_settings", job_id_index, level_id_index, difficulty_index, one_down)
-		end
+function SkirmishManager:on_end_assault()
+	local wave_number = self:current_wave_number()
+	local new_ransom_amount = tweak_data.skirmish.ransom_amounts[self:current_wave_number()]
+	self:set_ransom_amount(new_ransom_amount)
 
-		self._set_first_assault = true
+	wave_number = wave_number + 1
+
+	local new_difficulty = wave_difficulties[wave_number]
+
+	if new_difficulty and new_difficulty ~= Global.game_settings.difficulty then
+		Global.game_settings.difficulty = new_difficulty
+		tweak_data:set_difficulty(new_difficulty)
 	end
 
-	on_start_assault_original(self)
+	if Network:is_server() then
+		managers.network:session():send_to_peers("sync_end_assault_skirmish")
+	end
 end
 
-Hooks:PreHook(SkirmishManager, "on_end_assault", "SkirmishMod_IncreaseDifficulty", function(self)
-	local wave_number = self:current_wave_number() + 1
-	local difficulty = {
-		"normal", --1
-		"hard", --2
-		"overkill", --3
-		"overkill_145", --4
-		"overkill_145", --5
-		"easy_wish", --6
-		"overkill_290", --7
-		"overkill_290", --8
-		"sm_wish", --9
-		"sm_wish"
-	}
-	if difficulty[wave_number] then
-		local duff = difficulty[wave_number]
-		local job_id_index = tweak_data.narrative:get_index_from_job_id(managers.job:current_job_id())
-		local level_id_index = tweak_data.levels:get_index_from_level_id(Global.game_settings.level_id)
-		local difficulty_index = tweak_data:difficulty_to_index(duff)
-		local one_down = Global.game_settings.one_down and true or false		
-		Global.game_settings.difficulty = duff
-		tweak_data:set_difficulty(duff)
-
-		if managers.network then
-			managers.network:session():send_to_peers("sync_game_settings", job_id_index, level_id_index, difficulty_index, one_down)
-		end
+function SkirmishManager:sync_start_assault(wave_number)
+	if not self:is_skirmish() then
+		return
 	end
-end)
+
+	for i = (self._synced_wave_number or 0) + 1, wave_number do
+		self:_apply_modifiers_for_wave(i)
+	end
+
+	self._synced_wave_number = wave_number
+
+	local new_ransom_amount = tweak_data.skirmish.ransom_amounts[wave_number]
+	self:set_ransom_amount(new_ransom_amount)
+
+	local new_difficulty = wave_difficulties[wave_number]
+
+	if new_difficulty and new_difficulty ~= Global.game_settings.difficulty then
+		Global.game_settings.difficulty = new_difficulty
+		tweak_data:set_difficulty(new_difficulty)
+	end
+end
