9e6a8edf2b15569ec9fd1092420dff77d848aecd
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-04-01 08:35:09 -0300

    Update teamaimovement.lua

--- lua/lib/units/player_team/teamaimovement.lua
+++ lua/lib/units/player_team/teamaimovement.lua
@@ -1,3 +1,143 @@
+local mvec3_set = mvector3.set
+local tmp_vec1 = Vector3()
+local zero_vel_vec = Vector3(0, 0, 0)
+
+local mrot_lookat = mrotation.set_look_at
+local tmp_rot_1 = Rotation()
+
+local math_random = math.random
+local math_up = math.UP
+
+local type_g = type
+local next_g = next
+
+local clone_g = clone
+local alive_g = alive
+local call_on_next_update_g = call_on_next_update
+
+function TeamAIMovement:on_SPOOCed(enemy_unit)
+	local state = "incapacitated"
+	state = managers.modifiers:modify_value("PlayerMovement:OnSpooked", state)
+
+	if state == "arrested" then
+		self:on_cuffed()
+	else
+		self._unit:character_damage():on_incapacitated()
+	end
+
+	return true
+end
+
+function TeamAIMovement:pre_destroy()
+	TeamAIMovement.super.pre_destroy(self)
+
+	if self._heat_listener_clbk then
+		managers.groupai:state():remove_listener(self._heat_listener_clbk)
+
+		self._heat_listener_clbk = nil
+	end
+
+	if self._switch_to_not_cool_clbk_id then
+		managers.enemy:remove_delayed_clbk(self._switch_to_not_cool_clbk_id)
+
+		self._switch_to_not_cool_clbk_id = nil
+	end
+end
+
+function TeamAIMovement:carrying_bag(...)
+	return TeamAIMovement.super.carrying_bag(self, ...)
+end
+
+function TeamAIMovement:set_carrying_bag(...)
+	TeamAIMovement.super.set_carrying_bag(self, ...)
+end
+
+function TeamAIMovement:carry_id(...)
+	return TeamAIMovement.super.carry_id(self, ...)
+end
+
+function TeamAIMovement:carry_data(...)
+	return TeamAIMovement.super.carry_data(self)
+end
+
+function TeamAIMovement:carry_tweak(...)
+	return TeamAIMovement.super.carry_tweak(self)
+end
+
+function TeamAIMovement:throw_bag(...)
+	TeamAIMovement.super.throw_bag(self, ...)
+end
+
+function TeamAIMovement:was_carrying_bag(...)
+	return TeamAIMovement.super.was_carrying_bag(self, ...)
+end
+
+function TeamAIMovement:sync_throw_bag(...)
+	TeamAIMovement.super.sync_throw_bag(self, ...)
+end
+
+function TeamAIMovement:save(save_data)
+	TeamAIMovement.super.save(self, save_data)
+
+	local should_stay = self._should_stay
+
+	if should_stay ~= nil then
+		save_data.movement = save_data.movement or {}
+		save_data.movement.should_stay = should_stay
+	end
+end
+
+function TeamAIMovement:load(load_data)
+	TeamAIMovement.super.load(self, load_data)
+
+	local mov_load_data = load_data.movement
+
+	if not mov_load_data then
+		return
+	end
+
+	local should_stay = mov_load_data.should_stay
+
+	if should_stay ~= nil then
+		self:set_should_stay(should_stay)
+	end
+end
+
+function TeamAIMovement:set_should_stay(should_stay)
+	if self._should_stay == should_stay then
+		return
+	end
+
+	self._should_stay = should_stay
+
+	if Network:is_server() then
+		managers.network:session():send_to_peers_synched("sync_team_ai_stopped", self._unit, should_stay)
+	end
+
+	local panel = managers.criminals:character_data_by_unit(self._unit)
+
+	if panel then
+		managers.hud:set_ai_stopped(panel.panel_id, should_stay)
+	end
+end
+
+function TeamAIMovement:chk_action_forbidden(action_type)
+	--stay put orders are not supposed to affect clients
+	if action_type == "walk" and self._should_stay and Network:is_server() then
+		local objective = self._unit:brain():objective()
+
+		if objective then
+			if objective.forced or objective.type == "revive" then
+				return false
+			end
+		end
+
+		return true
+	end
+
+	return TeamAIMovement.super.chk_action_forbidden(self, action_type)
+end
+
 --used by clients
 function TeamAIMovement:sync_reload_weapon(empty_reload, reload_speed_multiplier)
 	local reload_action = {
@@ -15,22 +155,4 @@ function TeamAIMovement:is_taser_attack_allowed()
 	end
 	
 	return
-end
-
-function TeamAIMovement:pre_destroy()
-	--execute copmovement pre_destroy to ensure any changes to it also apply here
-	--this is how it should've been done from the start
-	TeamAIMovement.super.pre_destroy(self)
-
-	if self._heat_listener_clbk then
-		managers.groupai:state():remove_listener(self._heat_listener_clbk)
-
-		self._heat_listener_clbk = nil
-	end
-
-	if self._switch_to_not_cool_clbk_id then
-		managers.enemy:remove_delayed_clbk(self._switch_to_not_cool_clbk_id)
-
-		self._switch_to_not_cool_clbk_id = nil
-	end
 end
