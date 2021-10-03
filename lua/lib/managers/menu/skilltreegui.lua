24dfa9eb5c0e996058ce72ef19d6eeb2515ec259
offyerrocker <offyerrocker88@gmail.com>
2021-10-03 16:05:58 -0700

    (TCD) Sociopath card shake style changes, part 2
    Rotation transition is now smooth when selecting/deselecting a perk deck with the shake effect
    Fixed scanlines having 1-px horizontal overflow to the left of the card
    Lowered scanline opacity from 0.5 to 0.33
    Reduced size of scanlines, reduced random variance in size and allowed variance in both positive and directions
    Cleaned old commented code
    Changed some var/gui names for clarity

--- lua/lib/managers/menu/skilltreegui.lua
+++ lua/lib/managers/menu/skilltreegui.lua
@@ -1,3 +1,10 @@
+local angle = 10 --angle that the icon will rotate in either direction
+local rotation_speed = 0.5 --rotation cycles per second
+local time_offset = 0.33 --seconds delay for the shadow bitmap
+local rotation_offset = 0 --degrees delay for the shadow bitmap (functionally the same as time_offset but by a different measure)
+local scanlines_speed = 10 --px/sec
+
+
 Hooks:PostHook(SpecializationTierItem,"init","tcd_perkdeckgui_create",function(self,tier_data, tree_panel, tree, tier, x, y, w, h)
 
 	local td = tweak_data.skilltree.specializations[self._tree]
@@ -30,6 +37,7 @@ Hooks:PostHook(SpecializationTierItem,"init","tcd_perkdeckgui_create",function(s
 			texture_rect = texture_rect,
 			halign = "scale",
 			valign = "scale",
+			rotation = rotation_offset,
 			layer = 0,
 			color = Color("000000"),
 			alpha = 1/3
@@ -39,41 +47,55 @@ Hooks:PostHook(SpecializationTierItem,"init","tcd_perkdeckgui_create",function(s
 		tier_icon_shadow:move(0,4)
 		self._tier_icon_shadow = tier_icon_shadow
 		
+		--the original card texture is roughly 64x92, scaled down by 16px (?)
+		
 		local tier_scanlines_w = 64 - 8
 		local tier_scanlines_h = 92 - 8
-		local tier_scanlines = self._tier_panel:panel({
-			name = "tier_scanlines",
-			w = tier_scanlines_w,
+		local tier_scanlines_panel = self._tier_panel:panel({
+			name = "tier_scanlines_panel",
+			w = tier_scanlines_w - 2,
 			h = tier_scanlines_h,
 			x = (self._tier_panel:w() - tier_scanlines_w) / 2,
 			y = 5,
 			layer = 4
 		})
-		local scanlines = tier_scanlines:bitmap({
-			name = "scanlines",
+		local tier_scanlines = tier_scanlines_panel:bitmap({
+			name = "tier_scanlines",
 			texture = "guis/textures/pd2/damage_overlay_sociopath/scanlines_overlay",
 			texture_rect = {
-				0,math.random(360 - 92),1,84 + math.random(16)
+				0,math.random(360 - 92),1,92 + math.random(-4,4) --360 is the scanlines file height
 			},
-			w = tier_scanlines:w(),
-			h = tier_scanlines:h() * 2,
+			x = 1,
+			w = tier_scanlines_panel:w(),
+			h = tier_scanlines_panel:h() * 2,
 			y = -tier_scanlines_h,
 			blend_mode = "add",
 			halign = "scale",
 			valign = "scale",
-			alpha = 0.5,
+			alpha = 1/3,
 			layer = 10
 		})
-		self._tier_scanlines = scanlines
+		self._tier_scanlines = tier_scanlines
+		self._tier_scanlines_panel = tier_scanlines_panel
 		
 	end
 	
 end)
 
+local function lerp_rot(item,dt)
+	local rotation = item:rotation()
+
+	local lerp_rotation = math.lerp(rotation, 0, dt * 10)
+	local step_rotation = math.step(rotation, 0, dt * 20)
+
+	rotation = math.abs(rotation - lerp_rotation) < math.abs(rotation - step_rotation) and step_rotation or lerp_rotation
+
+	item:set_rotation(rotation)
+end
+				
 local orig_specializationtieritem_updatesize = SpecializationTierItem.update_size
 function SpecializationTierItem:update_size(dt, tree_selected, ...)
 	--todo smooth rotation/revert rotation when deselected
---[[
 	local size = {
 		self._tier_panel:size()
 	}
@@ -96,87 +118,40 @@ function SpecializationTierItem:update_size(dt, tree_selected, ...)
 	self._tier_panel:set_size(unpack(size))
 	self._tier_panel:set_center(cx, cy)
 	self._select_box_panel:set_center(self._tier_panel:w() / 2, self._tier_panel:h() / 2)
-
-	if not self._rotation_start_t then
-		self._rotation_start_t = Application:time()
-	end
-
-	local tweak_data = tweak_data.skilltree.specializations[self._tree]
-	if tweak_data.shake then
-		if tree_selected then
-			local time_since_start = Application:time() - self._rotation_start_t
-			local rotation = math.sin(time_since_start * 100) * 10
-
-			for _, child in pairs(self._tier_panel:children()) do
-				if child.set_rotation then
-					child:set_rotation(rotation)
-				end
-			end
-
-			return false
-		else
-			for _, child in pairs(self._tier_panel:children()) do
-				if child.set_rotation then
-					local rotation = child:rotation()
-					local lerp_rotation = math.lerp(rotation, 0, dt * 10)
-					local step_rotation = math.step(rotation, 0, dt * 20)
-					rotation = math.abs(rotation - lerp_rotation) < math.abs(rotation - step_rotation) and step_rotation or lerp_rotation
-
-					child:set_rotation(rotation)
-				end
-			end
-
-			self._rotation_start_t = nil
-		end
-	end
-
-	--]]
+	
 	local td = tweak_data.skilltree.specializations[self._tree]
 	if td.shake then
-		local t = Application:time()
-		local rotation_delay = 0.33 --seconds
-		local angle = 10
-		local scanlines_speed = 10 --px/sec
-		local rotation_speed = 0.5 --cycles per second
-		local is_done = false
-		
-		if alive(self._tier_icon) and alive(self._tier_icon_shadow) then 
-			if tree_selected then 
---				self._tier_icon:rotate(dt * rotation_speed * angle)
---				self._tier_icon_shadow:set_rotation(dt * rotation_speed * angle)
-				self._tier_icon:set_rotation(math.sin(t * 360 * rotation_speed) * angle)
-				self._tier_icon_shadow:set_rotation(math.sin((t - rotation_delay) * 360 * rotation_speed) * angle)
-				is_done = false
-			--[[
-				local desired_rotation = 0
-				local current_rotation = self._tier_icon:rotation()
-				local delta_rotation = dt * rotation_speed
-				if math.abs(desired_rotation - current_rotation) < math.abs(delta_rotation) then 
-					is_done = true
-					self._tier_icon:set_rotation(desired_rotation)
-					self._tier_icon_shadow:set_rotation(desired_rotation)
-				else
-					self._tier_icon:set_rotation(current_rotation + delta_rotation)
-					self._tier_icon_shadow:set_rotation(current_rotation + delta_rotation)
-					is_done = false
-				end
-				--]]
-			end
+		if not self._rotation_start_t then
+			self._rotation_start_t = Application:time()
 		end
+		
+		local t = Application:time() - self._rotation_start_t
 		
 		local tier_scanlines = self._tier_scanlines
 		if alive(tier_scanlines) then 
 			local scanline_y = tier_scanlines:y() + (dt * scanlines_speed)
 			if scanline_y >= 0 then 
-				tier_scanlines:set_y(-self._tier_panel:child("tier_scanlines"):h())
+				tier_scanlines:set_y(-self._tier_scanlines_panel:h())
 			else
 				tier_scanlines:set_y(scanline_y)
 			end
 			tier_scanlines:set_visible(tree_selected)
---			self._tier_panel:child("tier_scanlines"):set_x((self._tier_panel:w() - self._tier_panel:child("tier_scanlines"):w()) / 2)
+			self._tier_scanlines_panel:set_x((self._tier_panel:w() - self._tier_scanlines_panel:w()) / 2)
 		end
-		is_done = orig_specializationtieritem_updatesize(self,dt,tree_selected,...) and is_done
-		return not tree_selected and is_done
+		
+		if alive(self._tier_icon) and alive(self._tier_icon_shadow) then 
+			if tree_selected then 
+				self._tier_icon:set_rotation(math.sin(t * 360 * rotation_speed) * angle)
+				self._tier_icon_shadow:set_rotation(math.sin((t - time_offset) * 360 * rotation_speed) * angle)
+				return false
+			else
+				lerp_rot(self._tier_icon,dt)
+				lerp_rot(self._tier_icon_shadow,dt)
+				
+				self._rotation_start_t = nil
+			end
+		end
+		
 	else
 		return orig_specializationtieritem_updatesize(self,dt,tree_selected,...)
 	end
