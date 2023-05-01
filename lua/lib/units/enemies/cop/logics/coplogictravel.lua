2ac1cf6de5753cc3f29ea7cc5692f1995abb9473
offyerrocker <offyerrocker88@gmail.com>
2023-04-30 17:34:04 -0700

    cop _chk_say_clear crash fix
    temporarily disabled a cop voiceline in order to fix a crash

--- lua/lib/units/enemies/cop/logics/coplogictravel.lua
+++ lua/lib/units/enemies/cop/logics/coplogictravel.lua
@@ -775,12 +775,15 @@ function CopLogicTravel._chk_say_clear(data)
 	
 	if not data.entrance and data.char_tweak.chatter and data.char_tweak.chatter.entrance then
 		if not data.attention_obj or data.attention_obj.reaction <= AIAttentionObject.REACT_AIM or not data.attention_obj.verified_t or data.t - data.attention_obj.verified_t > 15 then
+		--[[
 			if data.unit:sound():say(data.char_tweak.chatter.entrance, true) then
 				data.entrance = true
 				
 				data.last_calm_chatter_t = data.t
 			end
 		end
+		--]]
+		data.last_calm_chatter_t = data.t --temp; remove this when this voiceline proc is reenabled
 	end
 end
 
