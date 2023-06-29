7349de1ff56f1c017a6bdaf7f87870099a3703db
offyerrocker <offyerrocker88@gmail.com>
2023-06-29 00:20:38 -0700

    Update mm keys
    Updated matchmaking keys
    Change the matchmaking key for Epic Games platform as well as Steam platform

---
+++ lua/lib/network/matchmaking/networkmatchmakingepic.lua
@@ -0,0 +1,5 @@
+if deathvox:IsTotalCrackdownEnabled() then 
+	NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = deathvox.mm_key_overhaul
+else
+	NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = deathvox.mm_key_default
+end
